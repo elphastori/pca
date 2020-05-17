5a84ea27e5294e911b962074dec9a4736ac7aaf6
Elphas <elphastori@gmail.com>
2020-05-17 18:04:01 +0000

    Added unit tests for covariance

--- test.cpp
+++ test.cpp
@@ -35,3 +35,26 @@ TEST_CASE("dimension mean") {
 	}
 
 }
+
+TEST_CASE("covariance") {
+	
+    vector<vector<double>> values = {{1.5, 2.5}, {2.5, 3.5}, {3.5, 4.5}};
+
+    SECTION("two dimensions (covariance)") {
+
+        double expected_covariance = 1.0;
+       
+	    REQUIRE(expected_covariance==covariance(values, 0, 1));
+	    REQUIRE(expected_covariance==covariance(values, 1, 0));
+	}
+
+    SECTION("one dimension (variance)") {
+
+        double expected_variance_0 = 1.0;
+        double expected_variance_1 = 1.0;
+       
+	    REQUIRE(expected_variance_0==covariance(values, 0, 0));
+	    REQUIRE(expected_variance_1==covariance(values, 1, 1));
+	}
+
+}
