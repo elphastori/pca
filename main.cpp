5039f7af328dae3ed386ba57c83857bf1d8aa23a
Elphas <elphastori@gmail.com>
2020-05-17 08:08:28 +0000

    Fixed error in reading covariance matrix

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ int main(int argc, char * argv[]) {
 
     // Display covariance matrix
     for (int i = 0; i < matrix.size(); ++i) {
-        for (int j = 0; matrix[i].size(); ++j) {
+        for (int j = 0; j < matrix[i].size(); ++j) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
