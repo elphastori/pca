9707fea609ca0a0f0921483d560cd7c5043175ed
Elphas <elphastori@gmail.com>
2020-05-17 07:46:25 +0000

    Added covariance matrix calculation

--- main.cpp
+++ main.cpp
@@ -9,8 +9,51 @@
 #include "utilities.h"
 
 using namespace std;
-
 using namespace TRNELP001;
+
+vector<vector<double>> mean_adjusted(vector<vector<double>> values) {
+    vector<vector<double>> normalized_values;
+
+    for (int i = 0; i < values.size(); ++i) {
+        vector<double> point;
+
+        for (int j = 0; j < values[i].size(); j++) {
+            double value = values[i][j] - dimension_mean(values, j);
+            point.push_back(value);
+        }
+
+        normalized_values.push_back(point);
+    }
+}
+
+double covariance(vector<vector<double>> values, int dim_a, int dim_b) {
+    int n = values.size();
+    double product_sums = 0;
+
+    for (int i = 0; i < n; ++i) {
+        double a_diff = (values[i][dim_a] - dimension_mean(values, dim_a));
+        double b_diff = (values[i][dim_b] - dimension_mean(values, dim_b));
+
+        product_sums += a_diff * b_diff;
+    }
+
+    return product_sums / (n - 1);
+}
+
+vector<vector<double>> covariance_matrix(vector<vector<double>> values) {
+    vector<vector<double>> matrix;
+    int n_dimensions = values[0].size();
+
+    for (int i = 0; i < 2; ++i) {
+        vector<double> row;
+        for (int j = 0; j < 2; ++j) {
+            row.push_back(covariance(values, i, j));
+        }
+        matrix.push_back(row);
+    }
+
+    return matrix;
+}
 
 int main(int argc, char * argv[]) {
 
@@ -36,9 +79,14 @@ int main(int argc, char * argv[]) {
     // Close file
     in.close();
 
-    // Verify loaded data
-    for (int i = 0; i < values.size(); ++i) {
-        cout << values[i][0] << " " << values[i][1] << endl;
+    vector<vector<double>> matrix = covariance_matrix(values);
+
+    // Display covariance matrix
+    for (int i = 0; i < matrix.size(); ++i) {
+        for (int j = 0; matrix[i].size(); ++j) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
     }
 
     return 0;
